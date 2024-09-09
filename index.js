6f95319cf6a25c601f16971b5132c973b093624a
cakradewahajji <74851963+cakradewahajji@users.noreply.github.com>
2024-09-09 15:43:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 3000;
 const deviceInfoRoute = require('./routes/deviceInfo');
 const initializeFilesRoute = require('./routes/files');
 const logsRoute = require('./routes/logs');  // Tambahkan ini
+const contactsRouter = require('./routes/contacts');
 const clearTempFolder = require('./routes/clearTemp');
 
 // Bersihkan folder temp saat server dimulai
@@ -17,6 +18,7 @@ app.use(express.static('public'));
 // Gunakan rute yang diimport
 app.use('/device-info', deviceInfoRoute);
 app.use('/logs', logsRoute);  // Tambahkan ini
+app.use(contactsRouter);
 
 async function startServer() {
   try {
